2aafddb57caedb6237105e3c1e54411d3b3250d5
Aeon Avinash <aeon.b.avinash@gmail.com>
2019-04-16 13:03:00 +0530

    today and tomorrow's high & low temp display

--- public/js/app.js
+++ public/js/app.js
@@ -1,21 +1,28 @@
 const weatherForm = document.querySelector('form');
 const displayDataTop = document.querySelector('.displayDataTop');
+const displayDataMiddle = document.querySelector('.displayDataMiddle');
 const displayDataBottom = document.querySelector('.displayDataBottom');
 
 weatherForm.addEventListener('submit', e => {
   e.preventDefault();
   const address = e.target.firstElementChild.value;
   displayDataTop.textContent = `Loading weather for ${address}...`;
+  displayDataMiddle.textContent = '';
   displayDataBottom.textContent = '';
   fetch(`/weather?address=${address}`)
     .then(res => res.json())
     .then(data => {
       if (data.error) {
         displayDataTop.textContent = data.error;
+        displayDataMiddle.textContent = '';
         displayDataBottom.textContent = '';
       } else {
         displayDataTop.textContent = data.location;
-        displayDataBottom.textContent = data.weather.forecast;
+        displayDataMiddle.textContent = data.weather.forecast.currently;
+        displayDataBottom.innerHTML =
+          data.weather.forecast.today +
+          '<br/>' +
+          data.weather.forecast.tomorrow;
       }
     })
     .catch(err => console.log(err));
--- src/utils/weather.js
+++ src/utils/weather.js
@@ -12,7 +12,19 @@ const fetchWeather = (lng, lat, callback) => {
         summary: body.daily.data[0].summary,
         temp: body.currently.temperature,
         precipProb: body.currently.precipProbability,
-        forecast: `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees C out. There is a ${body.currently.precipProbability} % chance of rain.`
+        forecast:  {
+          currently: `
+            ${body.daily.data[0].summary} \n
+            It is currently ${body.currently.temperature} C out. \n
+            There is a ${body.currently.precipProbability} % chance of rain. \n
+            `,
+          today: `
+            Today: High ${body.daily.data[0].temperatureHigh} C || ${body.daily.data[0].temperatureLow} C Low - ${body.daily.data[0].summary}
+            `,
+          tomorrow: `
+            Tomorrow: High ${body.daily.data[1].temperatureHigh} C || ${body.daily.data[1].temperatureLow} C Low - ${body.daily.data[1].summary} 
+            `,
+        }
       };
       callback(undefined, weatherData);
     }
